f5906fd3285ecd3f4be1df96731634b25950bba7
rntmfqlcsk <rntmfqlcsk@gmail.com>
2015-07-20 16:27:22 +0900

    HIDDEVICE

--- src/com/test/main.java
+++ src/com/test/main.java
@@ -1,85 +1,75 @@
 package com.test;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class main {
+import javax.swing.JOptionPane;
 
-	public static void main(String[] args) {
+import com.codeminders.hidapi.HIDDevice;
+import com.codeminders.hidapi.HIDManager;
+import com.hidapi.CallbackDeviceChange;
+import com.hidapi.DeviceChange;
+import com.hidapi.HidClassLoader;
+
+public class main implements DeviceChange{
 		
-		/*int a=10;
-		int sum=0;
+	static
+	{
+		if( !HidClassLoader.LoadLibrary())
+		{
+			JOptionPane.showMessageDialog(null,"This OS is not supported");
+		}
+	}
+	
+	private CallbackDeviceChange deviceChange =null;
+	private HIDManager manager =null;
+	public main()
+	{
+		 try {
+			manager =HIDManager.getInstance();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+		 deviceChange =  CallbackDeviceChange.getInstance(manager,this);
+		 deviceChange.setSerialNumber("MyPCR333333");
+		 deviceChange.start();
+	}
+	
+	private boolean statusFlag =true;
+
+	@Override
+	public void OnMessage(int MessageType, Object data, int firmwareVersion) {
 		
-		// sum = sum + i
-		// +=
-		for(int i=1; i<=a;sum += i, ++i)
-		;
+		// TODO Auto-generated method stub
+		String count = (String)data;
 		
-		int i=0;
-		
-		while(i <=a )
-		{ 
-			 sum += i;
-			 ++i;
+		switch(MessageType)
+		{
+			case CONNECTED:
+				if(count.equals("1")){
+				System.out.println("연결");
+				statusFlag= true;
+				}
+				break;
+			case DISCONNECTED:
+				if(count.equals("0"))
+				System.out.println("연결안됨");
+				statusFlag=! statusFlag;
+				break;
 		}
 		
-		System.out.println(sum);
-	}
-		
-		int[] arr= new int[10];
-		arr[0]=4;
-		arr[1]=6;
-		arr[2]=9;
-		arr[3]=3;
-		arr[4]=1;
-		arr[5]=2;
-		arr[6]=5;
-		arr[7]=15;
-		arr[8]=22;
-		arr[9]=8;
-		
-		Arrays.sort(arr);
-        
-		for (int i : arr )
-	
-			System.out.println(i);
-		
-		*/
-		
-		
-		/*int a = 50;
-        
-		for(int i=1; i<=a; ++i)
-		{
-			if( i%3 ==0 )
-			System.out.println(i);
-		}*/
-		
-		Scanner s= new Scanner( System.in);
-	
-		/*
-		3
-		0
-		1
-		4
-		 */
-        int t= s.nextInt();
-        
-        for(int i=0; i<t; ++i)
-        {
-        	int a =s.nextInt();
-        	int height=1;
-        	for (int j=1; j<=a; ++j)
-        	{
-        		if(j%2==0)
-        			height+=1;
-        		else
-        			height*=2;
-        	}
-        	System.out.println(height);
-        }
-        
-        
 	}	 
+	
+	
+	
+	
+	public static void main(String[] args) {
+		main m =new main();// 이렇게 하면 생성자가 호출됨
+		while(true);
+        
+	}
 }
 	
